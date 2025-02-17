eef963e04516f27046c856853801b7be87867db2
h.barati <hasanbaratiasp@gmail.com>
2025-02-17 15:55:57 +0330

    add home page grid template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
     const filters = (await searchParams); // Check if "Technology" exists
   return (
-    <div className=" relative" >
+    <div className=" relative " >
       <Navbar  />
      {filters.page ==="Home" && <HomePage />} 
     </div>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className='  h-svh bg-cover    bg-[url(/images/home/background-home-desktop.jpg)]'>
-      
+    <div className='  h-svh bg-cover flex flex-1 items-end   bg-[url(/images/home/background-home-desktop.jpg)]'>
+      <div className=' grid  grid-cols-2   text-center '>
+         <p className=' text-white'>sadsadsa</p>
+         <p className=' text-white'>sdsadsadsadsad</p>
+      </div>
     </div>
   )
 }
